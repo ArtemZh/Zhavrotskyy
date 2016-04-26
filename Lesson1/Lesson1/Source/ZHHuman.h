e3eb297f8ab6eb191912b9e50233a46225380690
Artem Zhavrotskiy <artemiyzh@gmail.com>
2016-04-26 20:03:32 +0300

    add Human.c

---
+++ Lesson1/Lesson1/Source/ZHHuman.h
@@ -0,0 +1,21 @@
+//
+//  ZHHuman.h
+//  Lesson1
+//
+//  Created by Artem Zhavrotskiy on 26.04.16.
+//  Copyright © 2016 Artem Zhavrotskiy. All rights reserved.
+//
+
+#ifndef ZHHuman_h
+#define ZHHuman_h
+
+#include <stdio.h>
+
+typedef enum {
+    ZHHumanGenderMale,
+    ZHHumanGenderFemale
+} ZHHumanGender;
+
+typedef struct ZHHuman ZHuman;
+
+#endif /* ZHHuman_h */
