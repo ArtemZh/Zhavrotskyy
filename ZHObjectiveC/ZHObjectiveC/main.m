3ee518d2e6835508349194349a883871e89f4eac
Artem Zhavrotskiy <artemiyzh@gmail.com>
2016-06-09 19:49:09 +0300

    add ZHObjectiveC folder

---
+++ ZHObjectiveC/ZHObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ZHObjectiveC
+//
+//  Created by Artem Zhavrotskiy on 09.06.16.
+//  Copyright © 2016 Artem Zhavrotskiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
